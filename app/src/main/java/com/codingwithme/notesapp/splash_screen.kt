5570b040f37f4f4125ef9e0f0ae18f8974a032d2
S Abhinav <sabhinav@S-Abhinav.local>
2021-01-22 23:04:12 +0530

    update

---
+++ app/src/main/java/com/codingwithme/notesapp/splash_screen.kt
@@ -0,0 +1,32 @@
+package com.codingwithme.notesapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.codingwithme.notesapp.MainActivity
+import com.codingwithme.notesapp.R
+
+@Suppress("DEPRECATION")
+class splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
